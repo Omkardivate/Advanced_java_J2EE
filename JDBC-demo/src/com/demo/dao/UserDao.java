59c6c3c3fa7b19fdadd0877ba9b213c81d5000cd
Omkardivate <112164147+Omkardivate@users.noreply.github.com>
2023-12-20 00:21:56 +0530

    JDBC userdata

---
+++ JDBC-demo/src/com/demo/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.demo.dao;
+
+import java.util.List;
+
+import com.demo.beans.User;
+
+public interface UserDao {
+
+	void save(String uname, String pwd);
+
+	List<User> getUsers();
+
+	List<String> getUsernames();
+
+	void removeByuname(String uname);
+
+}
